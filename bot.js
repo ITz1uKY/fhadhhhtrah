9985421961cf825bd7bd5f380bb7e59acd815ba5
ITz1uKY <42788278+ITz1uKY@users.noreply.github.com>
2018-11-23 23:33:27 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,44 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = "1";
+
+
+
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);  
+});
+
+client.on('ready', async() => {
+var server = "515615496910012418"; // ايدي السررفر
+var channel = "515616867663085569";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
+    },305);
+})
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+ 
+
+client.login(process.env.BOT_TOKEN)
